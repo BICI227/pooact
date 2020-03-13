820d1ba8aed22be0afb7f4479d8103b80ffb2605
BICI227 <60448107+BICI227@users.noreply.github.com>
2020-03-13 11:54:58 -0800

    Act para

---
+++ parametros/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace parametros
+{
+    class bisi
+    {
+        public static void bici(ref int a)
+        {
+            a++;
+        }
+        public static void birria(in int a,in int b, out int res)
+        {
+            res = a + b;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 2;
+            int res = 0;
+            
+            bisi.bici(ref x);
+            bisi.birria(in x, in y, out res);
+            Console.WriteLine(x);
+            Console.WriteLine(res);
+        }
+    }
+}
